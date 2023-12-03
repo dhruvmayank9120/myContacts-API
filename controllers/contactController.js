dd3baade05852a4c62870ed9653cdcaeceb979a2
Dhruv Mayank <dhruvmayank12@gmail.com>
2023-12-04 03:19:46 +0530

    first commit

---
+++ controllers/contactController.js
@@ -0,0 +1,82 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+//@desc Get all contacts
+//@route Get /api/contacts
+//@access public
+
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
+});
+
+//@desc CREATE new contact
+//@route POST /api/contacts
+//@access public
+
+const createContact = asyncHandler(async (req, res) => {
+  console.log("The request body is: ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("ALL FIELDS ARE MANDATORY !!");
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
+});
+
+//@desc GET contact
+//@route GET /api/contacts/:id
+//@access public
+
+const getContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res.status(200).json(contact);
+});
+
+//@desc update contact
+//@route PUT /api/contacts/:id
+//@access public
+
+const updateContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact no found to update");
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedContact);
+});
+
+//@desc delete contact
+//@route DELETE /api/contacts/:id
+//@access public
+
+const deleteContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact no found to delete");
+  }
+  await Contact.deleteOne({ _id: req.params.id });
+  res.status(200).json(contact);
+});
+
+module.exports = {
+  getContact,
+  createContact,
+  getContacts,
+  updateContact,
+  deleteContact,
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const errorHandler = require("./middleware/errorhandler");
+const connectDb = require("./config/dbConnection");
+const dotenv = require("dotenv").config();
+
+connectDb();
+
+const app = express();
+
+const port = process.env.PORT || 5000; // || sign means if process.env.port has some issues the default value will be 5000
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactsRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
